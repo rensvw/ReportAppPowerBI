16a79217b802a2cb800805ff86180b72b9c3af5c
rensvw <rensvanw@outlook.com>
2016-04-12 17:09:26 +0200

    Initial Commit

---
+++ app/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('casusMIT3', [
+    'ngRoute',
+    'casusMIT3.index',
+    'casusMIT3.results',
+    'ui.bootstrap',
+    'AdalAngular',
+    'ngMaterial'
+]).config(['$routeProvider', '$httpProvider', 'adalAuthenticationServiceProvider', '$sceDelegateProvider', function ($routeProvider, $httpProvider, adalAuthenticationServiceProvider, $sceDelegateProvider) {
+
+    $routeProvider.otherwise({
+        redirectTo: '/index'
+    });
+
+    adalAuthenticationServiceProvider.init({
+            tenant: "4c4da4a8-6a78-4ea2-8593-ae0608a6200b", // microsoft.onmicrosoft.com
+            clientId: "39596d89-8c4a-49ad-b299-69dbc7a7cd28", // Power BI AngularJS SPA
+            endpoints: {
+                'https://api.powerbi.com': 'https://analysis.windows.net/powerbi/api'
+            },
+            requireADLogin: true,
+            cacheLocation: 'localStorage'
+        },
+        $httpProvider
+    );
+
+    $sceDelegateProvider.resourceUrlWhitelist([
+        'self',
+        'https://*.powerbi.com/**'
+    ]);
+
+
+}]);
+//adafd
---
+++ app/dataFactory/powerBI.js
@@ -0,0 +1,73 @@
+/**
+ * Created by rensvanw on 4/7/2016.
+ */
+angular.module('casusMIT3')
+    .factory('powerBi', ['$http', function ($http) {
+
+        var urlBase = 'https://api.powerbi.com/v1.0/myorg';
+        var powerBi = {};
+
+        /// List Dashboards ///
+
+        powerBi.listAllDashboards = function () {
+            return $http.get(urlBase + '/dashboards');
+        };
+
+        /// Get tiles ///
+
+        powerBi.listAllTiles = function (dashboardID) {
+            return $http.get(urlBase + '/' + dashboardID + '/tiles');
+        };
+
+        powerBi.getTile = function (dashboardID, tileID) {
+            return $http.get(urlBase + '/' + dashboardID + '/tiles/' + tileID);
+        };
+
+        /// Get Reports //
+
+        powerBi.listAllReports = function () {
+            return $http.get(urlBase + '/reports');
+        };
+
+        /// DataSet Get + Post functions ///
+
+        powerBi.listAllDataSets = function () {
+            return $http.get(urlBase + '/datasets');
+        };
+
+        powerBi.CreateDataSet = function (dataset) {
+            return $http.post(urlBase + '/datasets?defaultRetentionPolicy=None', dataset);
+        };
+
+        /// Table functions ///
+
+        powerBi.listAllTables = function (id) {
+            return $http.get(urlBase + '/datasets/' + id + '/tables');
+        };
+
+        powerBi.addRowToTable = function (id, tableName, row) {
+            return $http.post(urlBase + '/datasets/' + id + '/tables/' + tableName + '/rows', row);
+
+        };
+
+        powerBi.updateExistingTable = function (id, tableName, item) {
+            return $http.put(urlBase + '/datasets/' + id + '/tables/' + tableName, item);
+        };
+
+        powerBi.clearRowsTable = function (id, tableName) {
+            return $http.delete(urlBase + '/datasets/' + id + '/tables/' + tableName + '/rows');
+        };
+
+        /// List Groups (Tile collections) ///
+
+        powerBi.listAllGroups = function () {
+            return $http.get(urlBase + '/groups');
+        };
+
+        return powerBi;
+
+    }
+
+
+    ]);
+
