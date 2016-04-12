9691bd2e591c2c786140e40b2a3f2c481a3ece9e
Rens van Woudenberg <rensvanw@outlook.com>
2016-04-13 00:12:51 +0200

    changed ad security

--- app/app.js
+++ app/app.js
@@ -17,8 +17,10 @@ angular.module('casusMIT3', [
     adalAuthenticationServiceProvider.init({
             tenant: "4c4da4a8-6a78-4ea2-8593-ae0608a6200b", // microsoft.onmicrosoft.com
             clientId: "39596d89-8c4a-49ad-b299-69dbc7a7cd28", // Power BI AngularJS SPA
+            redirectUri: "vwoudenberg.onmicrosoft.com/api",
             endpoints: {
-                'https://api.powerbi.com': 'https://analysis.windows.net/powerbi/api'
+                'https://api.powerbi.com': "https://analysis.windows.net/powerbi/api",
+                'https://moviewebapi.azurewebsites.net':"d8e0d928-a0b5-494f-9ea2-d3878d9362fa"
             },
             requireADLogin: true,
             cacheLocation: 'localStorage'
