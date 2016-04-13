944c4ddf06338546fcb29f1e7029c56832d7f5e9
Rens van Woudenberg <rensvanw@outlook.com>
2016-04-13 21:31:26 +0200

    changed ad security

--- app/index/index.js
+++ app/index/index.js
@@ -5,8 +5,7 @@ angular.module('casusMIT3.index', ['ngRoute', 'AdalAngular'])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/index', {
             templateUrl: 'index/index.html',
-            controller: 'indexCtrl',
-            requireADLogin: true
+            controller: 'indexCtrl'
         });
     }])
 
